4b7c7e5b034102122939ad7e280009100257e125
Senkathir Selvan <Senkathir.Selvan@tatacommunications.com>
2020-10-14 20:10:20 +0530

    Added user creation unit test

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test whether the user is successfully created with email"""
+        email = 'test@gmail.com'
+        password = 'Testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email,email)
+        self.assertTrue(user.check_password(password))#check_password is helper
+        #fn of get_user_model.this is used since pwd is hashed
