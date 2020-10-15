20079daaffc01fd580f84963d1ef1805d30ae66a
Senkathir Selvan <Senkathir.Selvan@tatacommunications.com>
2020-10-15 23:16:07 +0530

    django admin is customized

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
+#This is the general convention to convert any python string to human readable
+#text. It gets passed through translation engine(converts to other language)
+from core import models
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email','name']
+    fieldsets = (
+    #each bracket is a section
+        (None, {'fields':('email', 'password')}),#Title
+        (_('Personal Info'),{'fields':('name',)}),#comma used at end of params
+        #having one field otherwise it will be considered as string
+        (
+            _('Permissions'),
+            {'fields':('is_active','is_staff','is_superuser')}
+        ),
+        (_('Important dates'),{'fields':('last_login',)})
+    )
+    add_fieldsets = (
+        (None, {
+            'classes':('wide',),
+            'fields':('email','password1','password2')
+        }),
+    )
+
+admin.site.register(models.User, UserAdmin)
