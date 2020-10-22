9d2b546144137db591163ce4d0a21cf7010ab1ec
Senkathir Selvan <Senkathir.Selvan@tatacommunications.com>
2020-10-22 19:44:41 +0530

    Created serializer for generating token

--- app/user/urls.py
+++ app/user/urls.py
@@ -6,5 +6,6 @@ app_name = 'user'
 
 urlpatterns = [
     path('create/', views.CreateUserView.as_view(), name='create'),#name s used
-    #in reverse of test 
+    #in reverse of test
+    path('token/', views.CreateTokenView.as_view(), name='token'), 
 ]
